9af92467f06909828b0f25d9b86590159e3027bf
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2026-03-17 11:38:21 +0100

    chore: minor changes
    Signed-off-by: ale5000 <15793015+ale5000-git@users.noreply.github.com>

--- tools/check-app-updates.sh
+++ tools/check-app-updates.sh
@@ -95,9 +95,11 @@ dump_info_arg=''
 while test "${#}" -gt 0; do
   case "${1?}" in
     -V | --version)
+      # REUSE-IgnoreStart
       printf '%s\n' "${SCRIPT_NAME:?} v${SCRIPT_VERSION:?}"
-      printf '%s\n' "Copy""right (c) 2026 ${SCRIPT_AUTHOR:?}"
+      printf '%s\n' "Copyright (c) 2026 ${SCRIPT_AUTHOR:?}"
       printf '%s\n' 'License GPLv3+'
+      # REUSE-IgnoreEnd
       execute_script='false'
       ;;
 
